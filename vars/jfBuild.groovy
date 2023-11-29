44964ad16ab1d77d022bfca6985f71189e451a1b
ManiDevops21 <91456961+ManiDevops21@users.noreply.github.com>
2023-11-29 12:01:05 +0800

    Update jfBuild.groovy

--- vars/jfBuild.groovy
+++ vars/jfBuild.groovy
@@ -1,3 +1,4 @@
 def call(){
-  sh 'docker start artifactory'
+  echo '------excuting Jfrog'
+ echo """ curl -X PUT -u admin:Admin@01 -T /home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.01-SNAPSHOT.jar http://44.211.76.218:8082/artifactory/example-repo-local/  
 }
